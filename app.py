de47ec297f3022ed376b2517153976133fe5bc4c
karthiga096 <skarthigaaa96@gmail.com>
2026-01-13 21:15:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import random
 # Page Config
 # -----------------------------
 st.set_page_config(
-    page_title="❤️ Birthday Wishes for Munish",
+    page_title="❤️ Birthday Wishes for thango",
     page_icon="🎉",
     layout="centered"
 )
